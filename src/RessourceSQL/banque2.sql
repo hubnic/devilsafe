2e32c37d656ee0a75ab053f3ef624fad46a59aa2
Aissou <i7.aissou@gmail.com>
2017-03-01 15:31:19 -0500

    Mise à jour du script de BD

--- src/RessourceSQL/banque2.sql
+++ src/RessourceSQL/banque2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  127.0.0.1
--- Généré le :  Dim 26 Février 2017 à 16:40
+-- Généré le :  Mer 01 Mars 2017 à 20:30
 -- Version du serveur :  5.7.14
 -- Version de PHP :  5.6.25
 
@@ -29,31 +29,22 @@ USE `banque2`;
 --
 
 DROP TABLE IF EXISTS `administrateurs`;
-CREATE TABLE IF NOT EXISTS `administrateurs` (
-  `identifiant` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `administrateurs` (
+  `identifiant` int(11) NOT NULL,
   `nom` varchar(25) NOT NULL,
   `prenom` varchar(25) NOT NULL,
   `mdp` varchar(200) NOT NULL,
   `secureKey` int(11) NOT NULL,
-  `prefixe` varchar(1) NOT NULL DEFAULT 'a',
-  PRIMARY KEY (`identifiant`),
-  KEY `identifiant` (`identifiant`)
-) ENGINE=MyISAM AUTO_INCREMENT=1009 DEFAULT CHARSET=latin1;
+  `prefixe` varchar(1) NOT NULL DEFAULT 'a'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `administrateurs`
 --
 
 INSERT INTO `administrateurs` (`identifiant`, `nom`, `prenom`, `mdp`, `secureKey`, `prefixe`) VALUES
-(1000, 'nom', 'prenom', 'sadas', 1234, 'a'),
-(1001, 'Dup', 'Jean', 'j55vNuXGsZzlzjBYFy1k61me6FIyaTfgOy9tXK7U62bK68jonXH4OFATukKUHvtl', 1944, 'a'),
-(1002, 'Dup', 'Jean', 'BM69SPEjvnWcArAQpB6H2DCKDv+fWKmYAYICFNYnsfDrDHzW0UJ/9ZtKgfGYQvNG', 5512, 'a'),
-(1003, 'Dup', 'Jean', 'eJDoQD2gqaaWUG3h2afbcGTqIHg7e2UjKdKd7jdOJf//N0baqO6Wh3ToSnB3ObAm', 2740, 'a'),
-(1004, 'Idriss', 'Jean', 'YMZ0mDjJNrHSe5Jay+xnlsFByAw2wxZTlLwHesy7H5kxIKdJZzY45h9vogJ4aryO', 1320, 'a'),
-(1005, 'Dup', 'Jean', 'w6VxIbih4bqNh6+jbPVQ82kaHDLeRL3fAvEhlYoLajalKDWjBwb5e0qNss4MpBo8', 1087, 'a'),
-(1006, 'Dup', 'Jean', 'HnlXpxTxqeOkf0byMs81YNPyD4P/d/L1fQ6TCrNs0/wFggtz/li8sdGJuQ/rUVBy', 4407, 'a'),
-(1007, 'Dup', 'Jean', 'KvZMHNxknhrUTDCiybFEcgDfIyteTaWpKW6GJsl9V/5KfZR/V0OK4emPCMkNEXeQ', 7974, 'a'),
-(1008, 'Dup', 'Jean', 'dRcBHW05j1mUoCgSmVkkfTuGCHOAaHLBG1a4ptYw1l1yG6RDya0vJZHkkRx3VfWT', 2618, 'a');
+(1004, 'Administrateur', 'Boss', 'NK3FRsvo56mdu+twYrYob7JQFfERCoiAjn+j5MhwN9rvgad9Z+bMupMwXuA5mGiZ', 1320, 'a'),
+(1064, 'Administrateur', 'Administrateur', 'ydGRrbQfVocNmt63PWSIjuHVB5/6LJI+xz3Sl6pSLo1AAhpTKjG2PcVgCHYcdKOg', 4111, 'a');
 
 -- --------------------------------------------------------
 
@@ -62,14 +53,13 @@ INSERT INTO `administrateurs` (`identifiant`, `nom`, `prenom`, `mdp`, `secureKey
 --
 
 DROP TABLE IF EXISTS `carte`;
-CREATE TABLE IF NOT EXISTS `carte` (
+CREATE TABLE `carte` (
   `numCarte` varchar(16) NOT NULL,
   `dateExp` date NOT NULL,
   `crypto` int(11) NOT NULL,
   `nom` varchar(25) NOT NULL,
   `prenom` varchar(25) NOT NULL,
-  `idCompte` int(11) NOT NULL,
-  PRIMARY KEY (`numCarte`)
+  `idCompte` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -77,10 +67,13 @@ CREATE TABLE IF NOT EXISTS `carte` (
 --
 
 INSERT INTO `carte` (`numCarte`, `dateExp`, `crypto`, `nom`, `prenom`, `idCompte`) VALUES
-('6666121852784807', '2018-02-01', 54, 'Dup', 'Jean', 4),
-('666637979544334', '2018-02-01', 67, 'Dup', 'Jean', 1),
-('666654823457204', '2018-02-01', 463, 'Dup', 'Jean', 3),
-('6666719829994067', '2018-02-01', 361, 'Dup', 'Jean', 2);
+('6666148320307027', '2018-02-01', 563, 'Dupont', 'Jean', 6009),
+('6666268490386617', '2018-02-01', 722, 'Dup', 'Jean', 6002),
+('666648147628235', '2018-02-01', 35, 'Dupont', 'Jean', 6005),
+('666672319559253', '2018-02-01', 845, 'Dupont', 'Jean', 6011),
+('6666836142097970', '2018-02-01', 84, 'Dupont', 'Jean', 6007),
+('6666918583353599', '2018-02-01', 781, 'Dup', 'Jean', 6003),
+('6666943657236893', '2018-02-01', 901, 'Dupont', 'Jean', 6008);
 
 -- --------------------------------------------------------
 
@@ -89,8 +82,8 @@ INSERT INTO `carte` (`numCarte`, `dateExp`, `crypto`, `nom`, `prenom`, `idCompte
 --
 
 DROP TABLE IF EXISTS `clients`;
-CREATE TABLE IF NOT EXISTS `clients` (
-  `identifiant` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `clients` (
+  `identifiant` int(11) NOT NULL,
   `nom` varchar(25) NOT NULL,
   `prenom` varchar(25) NOT NULL,
   `courriel` varchar(100) NOT NULL,
@@ -98,25 +91,16 @@ CREATE TABLE IF NOT EXISTS `clients` (
   `telephone` varchar(12) NOT NULL,
   `adresse` varchar(600) NOT NULL,
   `mdp` varchar(200) NOT NULL,
-  `prefixe` varchar(1) NOT NULL DEFAULT 'c',
-  PRIMARY KEY (`identifiant`),
-  KEY `identifiant` (`identifiant`)
-) ENGINE=MyISAM AUTO_INCREMENT=9020 DEFAULT CHARSET=latin1;
+  `prefixe` varchar(1) NOT NULL DEFAULT 'c'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `clients`
 --
 
 INSERT INTO `clients` (`identifiant`, `nom`, `prenom`, `courriel`, `dateNaissance`, `telephone`, `adresse`, `mdp`, `prefixe`) VALUES
-(9008, 'Dup', 'Jean', 'jdup@j.com', '01-08-89', '512-234-4567', '12 rue v Montreal', '+Qk6Ob8kVXvkKVzrZvAmaS4cbLV6uiRGbKE+yZ52uFbjuCoBg6UxgFfIg8Dwo3cH', 'c'),
-(9009, 'Dup', 'Jean', 'jdup@j.com', '01-08-89', '512-234-4567', '12 rue v Montreal', 'XQhJAyB1J7BmWyPJC2vofuHgQ7j2KengLKyU++2iXILNURyAymjw19DsWyLb7Zi+', 'c'),
-(9010, 'utilisateur', 'Jean', 'jdup@j.com', '01-08-89', '512-234-4567', '12 rue v Montreal', 'gUx+NI6EjvyEXqT4v/VqKHXi7BdpS9IGeH/kNR9/x5pUrhmv8Rx/YFvTKzywE2jg', 'c'),
-(9019, 'Dup', 'Jean', 'jdup@j.com', '2017-02-26', '512-234-4567', '12 rue v Montreal', 'TwtlYiBcA7Qws0+5YjJMhr5dbyIJMT44n0KsXHJS8FeoXcSz1bm3hJhOo5/qhii3', 'c'),
-(9013, 'Dup', 'Jean', 'jdup@j.com', '01-08-89', '512-234-4567', '12 rue v Montreal', 'OX1mGV89Y/D84vv22l/VPhsiRaoxsK1Bs0rnp6xbK0dvRwkgODRreuoUHU7w8Ig2', 'c'),
-(9014, 'Dup', 'Jean', 'jdup@j.com', '01-08-89', '512-234-4567', '12 rue v Montreal', 'knsMxnPtGnaBmBH5uc8xrdG8exXSWqOQ38+ZEXe02lVEkFs0ektKsxGDFLqOOCi+', 'c'),
-(9015, 'Dup', 'Jean', 'jdup@j.com', '01-08-89', '512-234-4567', '12 rue v Montreal', 'rdnIKMQ8jydxhQXsHjmQqwVQ7IpoGSxnHbtu8tjyVa371KL+UwjzGYK6wNjHAs8F', 'c'),
-(9016, 'Dup', 'Jean', 'jdup@j.com', '01-08-89', '512-234-4567', '12 rue v Montreal', '/BrXfEuTrSZyJpgHX4M4tbvff2Kao90ad+iTlvF18orSfuwUSU7gmmSdE3APi0ee', 'c'),
-(9017, 'Dup', 'Jean', 'jdup@j.com', '2017-01-08', '512-234-4567', '12 rue v Montreal', 'Arx8sZDfuZIGS2ppcst7eJoUTRfe2RaYvEGz+jB5yqLDePto8hlrgf73MXuKzDSB', 'c');
+(9025, 'Dupont', 'Jean', 'membre@banque2.com', '2017-03-01', '512-234-4567', '12 rue v Montreal', 'ToXmtoFMPziuhBsDgLUrMQNeRPu8Z/VeQrn+GWQ6kRg7x6VX+1cTyGv0x2YBDH9v', 'c'),
+(9024, 'Dupont', 'Jean', 'membre@banque2.com', '2017-03-01', '512-234-4567', '12 rue v Montreal', '5XcimcZ5SoTOYDmWM+4mh+KvtBV/srakS4q2zad1mQvWzn8TpFpNCZfBkV49KZej', 'c');
 
 -- --------------------------------------------------------
 
@@ -125,46 +109,26 @@ INSERT INTO `clients` (`identifiant`, `nom`, `prenom`, `courriel`, `dateNaissanc
 --
 
 DROP TABLE IF EXISTS `compte`;
-CREATE TABLE IF NOT EXISTS `compte` (
-  `idCompte` int(5) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `compte` (
+  `idCompte` int(5) NOT NULL,
   `idBanque` varchar(4) NOT NULL DEFAULT '666-',
   `type` varchar(6) NOT NULL,
   `Solde` float NOT NULL,
-  `idClient` int(11) NOT NULL,
-  PRIMARY KEY (`idCompte`)
-) ENGINE=InnoDB AUTO_INCREMENT=10000 DEFAULT CHARSET=utf8;
+  `idClient` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `compte`
 --
 
 INSERT INTO `compte` (`idCompte`, `idBanque`, `type`, `Solde`, `idClient`) VALUES
-(1, '666-', 'Debit', 1234, 9008),
-(2, '666-', 'Credit', 1234, 9008),
-(3, '666-', 'Debit', 1234, 9017),
-(4, '666-', 'Credit', 1234, 9017);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `transaction`
---
-
-DROP TABLE IF EXISTS `transaction`;
-CREATE TABLE IF NOT EXISTS `transaction` (
-  `idTransaction` int(11) NOT NULL AUTO_INCREMENT,
-  `idCompteClient` int(11) NOT NULL,
-  `idCompteDestinataire` int(11) NOT NULL,
-  `montant` float NOT NULL,
-  `date` date NOT NULL,
-  `description` varchar(200) NOT NULL,
-  PRIMARY KEY (`idTransaction`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
+(6002, '666-', 'CREDIT', 0, 9023),
+(6003, '666-', 'DEBIT', 400, 9023),
+(6005, '666-', 'DEBIT', 1200, 9024),
+(6007, '666-', 'CREDIT', 0, 9025),
+(6008, '666-', 'DEBIT', 1500, 9025),
+(6009, '666-', 'DEBIT', 312, 9024),
+(6011, '666-', 'CREDIT', 0, 9024);
 
 -- --------------------------------------------------------
 
@@ -173,19 +137,119 @@ CREATE TABLE IF NOT EXISTS `transaction` (
 --
 
 DROP TABLE IF EXISTS `preautorisation`;
-CREATE TABLE IF NOT EXISTS `preautorisation` (
-  `preauth_id` int(11) NOT NULL AUTO_INCREMENT,
-  `credit_id` VARCHAR (16) NOT NULL ,
+CREATE TABLE `preautorisation` (
+  `preauth_id` int(11) NOT NULL,
+  `credit_id` int(11) NOT NULL,
   `credit_expiration` date NOT NULL,
   `credit_nom` varchar(25) NOT NULL,
   `credit_prenom` varchar(25) NOT NULL,
   `credit_cvs` varchar(3) NOT NULL,
-  `source_id` varchar(11) ,
-  `montant` DOUBLE NOT NULL,
-  `preauth_status` varchar(25) NOT NULL,
-  PRIMARY KEY (`preauth_id`)
+  `source_id` int(11) NOT NULL,
+  `montant` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `transaction`
+--
+
+DROP TABLE IF EXISTS `transaction`;
+CREATE TABLE `transaction` (
+  `idTransaction` int(11) NOT NULL,
+  `idCompteClient` int(11) NOT NULL,
+  `idCompteDestinataire` int(11) NOT NULL,
+  `montant` float NOT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `description` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `transaction`
+--
+
+INSERT INTO `transaction` (`idTransaction`, `idCompteClient`, `idCompteDestinataire`, `montant`, `date`, `description`) VALUES
+(1, 6003, 6002, 50, '2017-03-01 00:00:00', 'Ceci est mon test de transaction'),
+(2, 6003, 6002, 50, '2017-03-01 00:00:00', 'Ceci est mon test de transaction'),
+(3, 6003, 6002, 50, '2017-03-01 13:04:28', 'Ceci est mon test de transaction'),
+(4, 6005, 6009, -100, '2017-03-01 14:16:03', 'Virement vers compte 6009'),
+(5, 6009, 6005, 100, '2017-03-01 14:16:03', 'Virement en provenace du compte 6005'),
+(6, 6005, 6009, -100, '2017-03-01 15:21:05', 'Virement vers compte 6009'),
+(7, 6009, 6005, 100, '2017-03-01 15:21:05', 'Virement en provenace du compte 6005'),
+(8, 6005, 6009, -100, '2017-03-01 15:22:53', 'Virement vers compte 6009'),
+(9, 6009, 6005, 100, '2017-03-01 15:22:54', 'Virement en provenace du compte 6005');
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `administrateurs`
+--
+ALTER TABLE `administrateurs`
+  ADD PRIMARY KEY (`identifiant`),
+  ADD KEY `identifiant` (`identifiant`);
+
+--
+-- Index pour la table `carte`
+--
+ALTER TABLE `carte`
+  ADD PRIMARY KEY (`numCarte`);
+
+--
+-- Index pour la table `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`identifiant`),
+  ADD KEY `identifiant` (`identifiant`);
+
+--
+-- Index pour la table `compte`
+--
+ALTER TABLE `compte`
+  ADD PRIMARY KEY (`idCompte`);
+
+--
+-- Index pour la table `preautorisation`
+--
+ALTER TABLE `preautorisation`
+  ADD PRIMARY KEY (`preauth_id`);
+
+--
+-- Index pour la table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`idTransaction`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `administrateurs`
+--
+ALTER TABLE `administrateurs`
+  MODIFY `identifiant` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1065;
+--
+-- AUTO_INCREMENT pour la table `clients`
+--
+ALTER TABLE `clients`
+  MODIFY `identifiant` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9026;
+--
+-- AUTO_INCREMENT pour la table `compte`
+--
+ALTER TABLE `compte`
+  MODIFY `idCompte` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6012;
+--
+-- AUTO_INCREMENT pour la table `preautorisation`
+--
+ALTER TABLE `preautorisation`
+  MODIFY `preauth_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `idTransaction` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
